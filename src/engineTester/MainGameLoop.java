eea91f793b4b142df45765d7c52548725d612d53
ThiagoYonamine <thyonamine1@gmail.com>
2017-01-22 17:18:26 -0200

    Rederizando 10 dragoes juntos com reflexo e textura

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -1,5 +1,9 @@
 package engineTester;
  
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 import models.RawModel;
 import models.TexturedModel;
  
@@ -8,6 +12,7 @@ import org.lwjgl.util.vector.Vector3f;
  
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
+import renderEngine.MasterRender;
 import renderEngine.OBJLoader;
 import renderEngine.Renderer;
 import shaders.StaticShader;
@@ -22,37 +27,55 @@ public class MainGameLoop {
  
         DisplayManager.createDisplay();
         Loader loader = new Loader();
-        StaticShader shader = new StaticShader();
-        Renderer renderer = new Renderer(shader);
+        //StaticShader shader = new StaticShader();
+        //Renderer renderer = new Renderer(shader);
          
-
-        RawModel model = OBJLoader.loadObjModel("dragon", loader);
-         
+        //carrega o dragon
+        RawModel model = OBJLoader.loadObjModel("dragon", loader);   
         TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("grass")));
+        //set as propriedades da textura
         ModelTexture texture = staticModel.getTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(2);
         
+        
+        //Cria o dragon
         //entity (textura,new Vector3f(POSITION),ROTATION, SCALE);
-        Entity entity = new Entity(staticModel, new Vector3f(0,-5,-25),0,0,0,1);
-        //Luz (location, color)
+         Entity entity = new Entity(staticModel, new Vector3f(0,-5,-25),0,0,0,1);
+        //Cria Luz (location, color)
          Light light =new Light(new Vector3f(200,200,100),new Vector3f(1,1,1));
         
         Camera camera = new Camera();
-         
+        
+        List<Entity> allDragons = new ArrayList<Entity>();
+        Random random = new Random();
+        for(int i =0 ; i<10;i++){
+        	float x = random.nextFloat()*50-20;
+        	float y = random.nextFloat()*50-20;
+        	float z = random.nextFloat()*-100;
+        	allDragons.add(new Entity(staticModel, new Vector3f(x,y,z),random.nextFloat()*180f,random.nextFloat()*180f,0,1));
+        }
+        MasterRender renderer = new MasterRender();
         while(!Display.isCloseRequested()){
-            entity.increaseRotation(0, 1, 0);
+            
             camera.move();
-            renderer.prepare();
-            shader.start();  
-            shader.loadLight(light);
-            shader.loadViewMatrix(camera);
-            renderer.render(entity,shader);
-            shader.stop();
+            /* Tudo foi colocado no MasterRender
+           // renderer.prepare();
+           // shader.start();  
+           // shader.loadLight(light);
+            //shader.loadViewMatrix(camera);
+           // renderer.render(entity,shader);
+            //shader.stop();*/
+            
+            for (Entity dragon: allDragons){
+            	dragon.increaseRotation(0, 1, 0);
+            	renderer.processEntity(dragon);
+            }
+            renderer.render(light, camera);
             DisplayManager.updateDisplay();
         }
- 
-        shader.cleanUp();
+        renderer.cleanUp();
+        //shader.cleanUp();
         loader.cleanUp();
         DisplayManager.closeDisplay();
  
--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -1,5 +1,8 @@
 package renderEngine;
  
+import java.util.List;
+import java.util.Map;
+
 import models.RawModel;
 import models.TexturedModel;
  
@@ -25,8 +28,10 @@ public class Renderer {
     private static final float FAR_PLANE = 1000;
      
     private Matrix4f projectionMatrix;
+    private StaticShader shader;
      
     public Renderer(StaticShader shader){
+    	this.shader = shader;
     	GL11.glEnable(GL11.GL_CULL_FACE);
     	GL11.glCullFace(GL11.GL_BACK);
         createProjectionMatrix();
@@ -41,7 +46,48 @@ public class Renderer {
         GL11.glClearColor(0.4f, 0.3f, 0.0f, 1);
     }
  
-    public void render(Entity entity, StaticShader shader) {
+    public void render(Map<TexturedModel,List<Entity>> entities){
+    	for(TexturedModel model : entities.keySet()){
+    		prepareTexturedModel(model);
+    		List<Entity> batch = entities.get(model);
+    		for(Entity entity: batch){
+    			prepareInstance(entity);
+    			GL11.glDrawElements(GL11.GL_TRIANGLES, model.getRawModel().getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
+    		}
+    		unbindTexturedModel();
+    	}
+    	
+    }
+    private void prepareTexturedModel(TexturedModel model){
+    	RawModel rawModel = model.getRawModel();
+        GL30.glBindVertexArray(rawModel.getVaoID());
+        GL20.glEnableVertexAttribArray(0);
+        GL20.glEnableVertexAttribArray(1);
+        GL20.glEnableVertexAttribArray(2);
+        ModelTexture texture = model.getTexture();
+        shader.loadShineVariables(texture.getShineDamper(), texture.getReflectivity());
+        GL13.glActiveTexture(GL13.GL_TEXTURE0);
+        GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getID());
+        
+    	
+    }
+    private void unbindTexturedModel(){
+    	GL20.glDisableVertexAttribArray(0);
+        GL20.glDisableVertexAttribArray(1);
+        GL20.glDisableVertexAttribArray(2);
+        GL30.glBindVertexArray(0);
+    	
+    }
+    private void prepareInstance(Entity entity){
+    	Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(),
+                entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
+        shader.loadTransformationMatrix(transformationMatrix);
+        
+    	
+    }
+    
+    //Dividimos essa funcao em 3 partes, para otimizar o render de varios objetos
+    /*public void render(Entity entity, StaticShader shader) {
         TexturedModel model = entity.getModel();
         RawModel rawModel = model.getRawModel();
         GL30.glBindVertexArray(rawModel.getVaoID());
@@ -60,7 +106,7 @@ public class Renderer {
         GL20.glDisableVertexAttribArray(1);
         GL20.glDisableVertexAttribArray(2);
         GL30.glBindVertexArray(0);
-    }
+    }*/
      
     private void createProjectionMatrix(){
         float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
