fbbb49bfb01d4883588a3c0521176d5f3fbea160
ThiagoYonamine <thyonamine1@gmail.com>
2017-01-22 16:23:51 -0200

    reflexo com textura melhorado

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -28,7 +28,7 @@ public class MainGameLoop {
 
         RawModel model = OBJLoader.loadObjModel("dragon", loader);
          
-        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("white")));
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("grass")));
         ModelTexture texture = staticModel.getTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(2);
@@ -36,7 +36,7 @@ public class MainGameLoop {
         //entity (textura,new Vector3f(POSITION),ROTATION, SCALE);
         Entity entity = new Entity(staticModel, new Vector3f(0,-5,-25),0,0,0,1);
         //Luz (location, color)
-         Light light =new Light(new Vector3f(0,0,-20),new Vector3f(1,1,1));
+         Light light =new Light(new Vector3f(200,200,100),new Vector3f(1,1,1));
         
         Camera camera = new Camera();
          
