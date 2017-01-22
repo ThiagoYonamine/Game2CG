b1cf0f9d7529da23131b0e5a3c65179c40e65de6
ThiagoYonamine <thyonamine1@gmail.com>
2017-01-22 03:32:34 -0200

    Cubo com textura, matriz de transformacao e camera

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,130 @@
+package engineTester;
+ 
+import models.RawModel;
+import models.TexturedModel;
+ 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.util.vector.Vector3f;
+ 
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import Textures.ModelTexture;
+import entities.Camera;
+import entities.Entity;
+ 
+public class MainGameLoop {
+ 
+    public static void main(String[] args) {
+ 
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        StaticShader shader = new StaticShader();
+        Renderer renderer = new Renderer(shader);
+         
+        float[] vertices = {            
+                -0.5f,0.5f,0,   
+                -0.5f,-0.5f,0,  
+                0.5f,-0.5f,0,   
+                0.5f,0.5f,0,        
+                 
+                -0.5f,0.5f,1,   
+                -0.5f,-0.5f,1,  
+                0.5f,-0.5f,1,   
+                0.5f,0.5f,1,
+                 
+                0.5f,0.5f,0,    
+                0.5f,-0.5f,0,   
+                0.5f,-0.5f,1,   
+                0.5f,0.5f,1,
+                 
+                -0.5f,0.5f,0,   
+                -0.5f,-0.5f,0,  
+                -0.5f,-0.5f,1,  
+                -0.5f,0.5f,1,
+                 
+                -0.5f,0.5f,1,
+                -0.5f,0.5f,0,
+                0.5f,0.5f,0,
+                0.5f,0.5f,1,
+                 
+                -0.5f,-0.5f,1,
+                -0.5f,-0.5f,0,
+                0.5f,-0.5f,0,
+                0.5f,-0.5f,1
+                 
+        };
+         
+        float[] textureCoords = {
+                 
+                0,0,
+                0,1,
+                1,1,
+                1,0,            
+                0,0,
+                0,1,
+                1,1,
+                1,0,            
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0,
+                0,0,
+                0,1,
+                1,1,
+                1,0
+ 
+                 
+        };
+         
+        int[] indices = {
+                0,1,3,  
+                3,1,2,  
+                4,5,7,
+                7,5,6,
+                8,9,11,
+                11,9,10,
+                12,13,15,
+                15,13,14,   
+                16,17,19,
+                19,17,18,
+                20,21,23,
+                23,21,22
+ 
+        };
+         
+        RawModel model = loader.loadToVAO(vertices,textureCoords,indices);
+         
+        TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("tree")));
+         
+        Entity entity = new Entity(staticModel, new Vector3f(0,0,-5),0,0,0,1);
+         
+        Camera camera = new Camera();
+         
+        while(!Display.isCloseRequested()){
+            entity.increaseRotation(1, 1, 0);
+            camera.move();
+            renderer.prepare();
+            shader.start();
+            shader.loadViewMatrix(camera);
+            renderer.render(entity,shader);
+            shader.stop();
+            DisplayManager.updateDisplay();
+        }
+ 
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+ 
+    }
+ 
+}
---
+++ src/models/TexturedModel.java
@@ -0,0 +1,31 @@
+package models;
+
+import Textures.ModelTexture;
+
+public class TexturedModel {
+	
+	private RawModel rawModel;
+	private ModelTexture texture;
+	
+	public TexturedModel(RawModel rawModel, ModelTexture texture){
+		this.rawModel = rawModel;
+		this.texture = texture;
+	}
+
+	public RawModel getRawModel() {
+		return rawModel;
+	}
+
+	public void setRawModel(RawModel rawModel) {
+		this.rawModel = rawModel;
+	}
+
+	public ModelTexture getTexture() {
+		return texture;
+	}
+
+	public void setTexture(ModelTexture texture) {
+		this.texture = texture;
+	}
+
+}
