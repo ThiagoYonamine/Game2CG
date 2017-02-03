ea6a388f6e330cc3ee5915facdfcc43cad239f09
ThiagoYonamine <thyonamine1@gmail.com>
2017-02-03 20:57:31 -0200

    Teste de colisão

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -42,12 +42,13 @@ public class MainGameLoop {
 
 		RawModel model = OBJLoader.loadObjModel("pine", loader);
 		TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("pine")));
+		staticModel.getTexture().setHasTransparency(true);
 		List<Entity> entities = new ArrayList<Entity>();
 		Random random = new Random();
-		for (int i = 0; i < 500; i++) {
+		for (int i = 0; i < 200; i++) {
 			float scale = MIN_TREE_HEIGHT + (float) Math.random() * (MAX_TREE_HEIGHT - MIN_TREE_HEIGHT);
 			entities.add(new Entity(staticModel,
-					new Vector3f(random.nextFloat() * 800 - 400, 0, random.nextFloat() * -600), 0, 0, 0, scale));
+					new Vector3f(random.nextFloat() * 800 - 400, 0, (random.nextFloat() * -600)-30), 0, 0, 0, scale));
 		}
 
 		RawModel model2 = OBJLoader.loadObjModel("fern", loader);
@@ -61,16 +62,21 @@ public class MainGameLoop {
 			entities2.add(new Entity(staticModel2,
 					new Vector3f(random.nextFloat() * 800 - 400, -1f, random.nextFloat() * -600), 0, 0, 0, scale/10));
 		}
-
+		/*
+		RawModel model_tiro = OBJLoader.loadObjModel("pine", loader);
+		TexturedModel tx_tiro = new TexturedModel(model_tiro, new ModelTexture(loader.loadTexture("pine")));
+		List<Entity> tiros = new ArrayList<Entity>();
+		tiros.add(new Entity(tx_tiro,new Vector3f(1, 1f, 1), 0, 0, 0, 1));
+		*/
+		
 		RawModel dragonOBJ = OBJLoader.loadObjModel("dragon", loader);
 		TexturedModel dragon = new TexturedModel(dragonOBJ, new ModelTexture(loader.loadTexture("verde")));
 		ModelTexture dragon_texture = dragon.getTexture();
 		// Reflexo
-		dragon_texture.setShineDamper(50); // tipo do material
-		dragon_texture.setReflectivity(200); // reflexo
+		dragon_texture.setShineDamper(10); // tipo do material
+		dragon_texture.setReflectivity(50); // reflexo
 
-		// Entity entityDragon = new Entity(dragon, new Vector3f(0, 0, -40), 0,
-		// 0, 0, 1);
+		Entity entityDragon = new Entity(dragon, new Vector3f(0, 0, -40), 0, 0, 0, 1);
 		
 		RawModel model_arma = OBJLoader.loadObjModel("arma", loader);
 		TexturedModel tx_arma = new TexturedModel(model_arma, new ModelTexture(loader.loadTexture("arma")));
@@ -91,27 +97,52 @@ public class MainGameLoop {
 		MasterRender renderer = new MasterRender();
 		TexturedModel player_model = tx_arma;
 
-		Player player = new Player(player_model, new Vector3f(100, 5, -50), 0, 0, 0, 0.5f);
+		Player player = new Player(player_model, new Vector3f(0, 5, 0), 0, 0, 0, 0.5f);
 		player.increaseRotation(180, 0, 0);
 		
 
 		// hide the mouse
 		Mouse.setGrabbed(true);
 		
+		boolean andar = true;
 		while (!Display.isCloseRequested()) {
+			if(andar)
 			player.move();
 
 			renderer.processEntity(player);
 			renderer.processTerrain(terrain);
 			renderer.processTerrain(terrain2);
 			for (Entity entity : entities) {
+				Vector3f v = entity.getPosition();
 				renderer.processEntity(entity);
+				
+				//todo ??? colisõa
+				/*float px = player.getPosition().getX();
+				float py = player.getPosition().getY();
+				float pz = player.getPosition().getZ();
+				if((pz <= v.getZ()-2  && px <= v.getX()+2 && px >= v.getX()-2) ){
+					andar = false;
+				}
+				else{
+					andar = true;
+				}*/
 			}
 			for (Entity entity2 : entities2) {
 				renderer.processEntity(entity2);
 			}
+			
+			
 			// entityDragon.increaseRotation(0, 1, 0);
-			// renderer.processEntity(entityDragon);
+			renderer.processEntity(entityDragon); // 0 0 -40
+			//teste colisao
+			float px = player.getPosition().getX();
+			float pz = player.getPosition().getZ();
+			if(pz <= -35 && px >= -5 && px <= 5 && pz >= -45){
+				
+				player.increasePosition(0, 0, 3);
+				
+			}
+			
 			entityArma.setPosition(new Vector3f(player.getPosition().x,player.getPosition().y,player.getPosition().z));
 			entityArma.setRotY(player.getRotY()*-1);
 			renderer.processEntity(entityArma);
